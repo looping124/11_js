301bbd3c05a69ef7ea14da78e17604a74c4afe02
Cedric Le Net <lenetcedric@gmail.com>
2022-01-03 11:55:18 +0100

    exo 1

---
+++ index.js
@@ -0,0 +1,70 @@
+let btnScript_1 = document.querySelector('.script-1');
+let btnAns_1 = document.querySelector('.script-1-answer');
+const entrepreneurs = [
+  { first: 'Steve', last: 'Jobs', year: 1955 },
+  { first: 'Oprah', last: 'Winfrey', year: 1954 },
+  { first: 'Bill', last: 'Gates', year: 1955 },
+  { first: 'Sheryl', last: 'Sandberg', year: 1969 },
+  { first: 'Mark', last: 'Zuckerberg', year: 1984 },
+  { first: 'Beyonce', last: 'Knowles', year: 1981 },
+  { first: 'Jeff', last: 'Bezos', year: 1964 },
+  { first: 'Diane', last: 'Hendricks', year: 1947 },
+  { first: 'Elon', last: 'Musk', year: 1971 },
+  { first: 'Marissa', last: 'Mayer', year: 1975 },
+  { first: 'Walt', last: 'Disney', year: 1901 },
+  { first: 'Larry', last: 'Page', year: 1973 },
+  { first: 'Jack', last: 'Dorsey', year: 1976 },
+  { first: 'Evan', last: 'Spiegel', year: 1990 },
+  { first: 'Brian', last: 'Chesky', year: 1981 },
+  { first: 'Travis', last: 'Kalanick', year: 1976 },
+  { first: 'Marc', last: 'Andreessen', year: 1971 },
+  { first: 'Peter', last: 'Thiel', year: 1967 }
+];
+const entrepreneurs2 = [
+  { first: 'Steve', last: 'Jobs', year: 1955 },
+  { first: 'Oprah', last: 'Winfrey', year: 1954 },
+  { first: 'Bill', last: 'Gates', year: 1955 },
+  { first: 'Sheryl', last: 'Sandberg', year: 1969 },
+  { first: 'Mark', last: 'Zuckerberg', year: 1984 },
+  { first: 'Beyonce', last: 'Knowles', year: 1981 },
+  { first: 'Jeff', last: 'Bezos', year: 1964 },
+  { first: 'Diane', last: 'Hendricks', year: 1947 },
+  { first: 'Elon', last: 'Musk', year: 1971 },
+  { first: 'Marissa', last: 'Mayer', year: 1975 },
+  { first: 'Walt', last: 'Disney', year: 1901 },
+  { first: 'Larry', last: 'Page', year: 1973 },
+  { first: 'Jack', last: 'Dorsey', year: 1976 },
+  { first: 'Evan', last: 'Spiegel', year: 1990 },
+  { first: 'Brian', last: 'Chesky', year: 1981 },
+  { first: 'Travis', last: 'Kalanick', year: 1976 },
+  { first: 'Marc', last: 'Andreessen', year: 1971 },
+  { first: 'Peter', last: 'Thiel', year: 1967 }
+];
+
+btnScript_1.addEventListener('click', e => {
+  entrepreneursWithoutYear = entrepreneurs
+  entrepreneursWithAge = entrepreneurs2
+
+  entrepreneursWithoutYear.forEach(entrepreneur => delete entrepreneur['year']);
+  console.log(entrepreneursWithoutYear);
+  entrepreneursWithAge
+  year = new Date().getFullYear()
+  entrepreneursWithAge.forEach(entrepreneur => entrepreneur['year'] = (year-entrepreneur['year']));
+  console.log(entrepreneursWithAge);
+
+  entrepreneursWithAge.forEach(entrepreneur => script3(entrepreneur) );
+  console.log(entrepreneursWithAge);
+
+  const entrepreneurOlderThan50 = entrepreneursWithAge.filter(entrepreneur =>  entrepreneur.age > 49)
+  console.log(entrepreneurOlderThan50);
+})
+
+function script3(entrepreneur) {
+  entrepreneur.firstName = entrepreneur.first;
+  delete entrepreneur.first;
+
+  entrepreneur.lastName = entrepreneur.last;
+  delete entrepreneur.last;
+  entrepreneur.age = entrepreneur.year;
+  delete entrepreneur.year;
+}
