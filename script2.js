6ba7d5f7f8d21752dee56fe9cd44222b9be8f081
Cedric Le Net <lenetcedric@gmail.com>
2022-01-03 11:58:55 +0100

    init script 2

---
+++ script2.js
@@ -0,0 +1,6 @@
+let btnScript_2 = document.querySelector('.script-2');
+let btnAns_2 = document.querySelector('.script-2-answer');
+
+btnScript_2.addEventListener('click', e => {
+  console.log("Bonjour ca va ?");
+})
